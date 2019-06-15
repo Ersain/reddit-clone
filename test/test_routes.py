b49240b5df351fbe84412de9af01e3642b033b42
MagiskBoy <37865517+MagiskBoy@users.noreply.github.com>
2019-06-15 21:03:57 +0600

    Add coverage

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,6 +1,10 @@
+import sys
+sys.path.append("..")
+
 from flask import Flask
 from reddit import app
 from reddit.post import routes as post_routes
+from reddit.user import routes as user_routes
 
 
 def test_home_route():
@@ -30,6 +34,30 @@ def test_user_route():
 def test_post_route():
     client = app.test_client()
     url = '/post/c0i68l'
-
+    
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_search_route_1():
+    client = app.test_client()
+    url = '/search/subreddit'
+    
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+def test_search_route_2():
+    client = app.test_client()
+    url = '/search/user'
+    
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+test_home_route()
+test_subreddit_route()
+test_user_route()
+test_post_route()
+test_search_route_1()
+test_search_route_2()
